b8445e797adf8730fc9e7abcf5437facb76379ac
Henny2 <henrieke.baunack@web.de>
2023-11-24 19:13:00 -0800

    Initial Commit

---
+++ NavigationDestinationPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavigationDestinationPractice
+//
+//  Created by Henrieke Baunack on 11/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationDestinationPractice/NavigationDestinationPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationDestinationPracticeApp.swift
+//  NavigationDestinationPractice
+//
+//  Created by Henrieke Baunack on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationDestinationPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
